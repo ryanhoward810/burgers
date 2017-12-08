a18d63d65bb5f62778a6d79aae1dcd38fb7db203
Ryan Howard <ryanhoward810@gmail.com>
2017-12-08 14:42:08 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 var mysql = require('mysql');
 var connection;
-// var host = process.env.CLEARDB_DATABASE_URL || '127.0.0.1';
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
-   host     : 'localhost',
+    host     : 'localhost',
     user     : 'root',
     password : 'Mayonaise1!',
     database : 'burgers_db'
